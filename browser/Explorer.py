6ca3789274f675d44bc230adfd7c69196cbd045a
Tiaonmmn <tiaonmmn@163.com>
2019-03-23 18:31:33 +0800

    Making codes.

--- browser/Explorer.py
+++ browser/Explorer.py
@@ -1,7 +1,8 @@
-import os
 import datetime
-from loguru import logger
+import os
+
 from Registry import Registry
+from loguru import logger
 
 
 class Explorer:
@@ -46,3 +47,68 @@ class Explorer:
                     logger.info("KeyWords: %s" % (item.value().decode('utf-16-le')))
         logger.critical(result)
         return result
+
+    def getRunMRU(self):
+        output = self.volumeInfo
+        try:
+            bias = datetime.timedelta(hours=-self.bias)
+        except TypeError:
+            pass
+        if "FAT" or "NTFS" in output.split(" ")[0]:
+            os.chdir("%s/%s/" % (self.mountDir, output.split(" ")[2]))
+            logger.info("Loading every user info!")  # TODO:It should be per user!
+            try:
+                os.chdir("Users/")
+            except FileNotFoundError:
+                logger.error("Couldn't find Users folder!")
+                return None
+            for userDir in os.listdir("."):
+                if os.access("{0}/NTUSER.DAT".format(userDir), os.F_OK | os.R_OK):
+                    registry = Registry.Registry("{0}/NTUSER.DAT".format(userDir))
+                elif os.access("{0}/ntuser.dat".format(userDir), os.F_OK | os.R_OK):
+                    registry = Registry.Registry("{0}/ntuser.dat".format(userDir))
+                else:
+                    logger.warning("Couldn't find user registry on %s" % userDir)
+                    continue
+                open1 = registry.open("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\RunMRU")
+                networkIP = []
+                logger.info("Showing Run MRU info.")
+                for item in open1.values():
+                    if item.name() == "MRUList":
+                        continue
+                    logger.info(open1.value())
+                    if "\\\\" in open1.value():
+                        networkIP.append(open1.value())
+                logger.info("Last run time:" + (open1.timestamp() + bias).strftime("%Y %m %d - %H:%M:%S"))
+        logger.critical("Found suspected network drive.%s" % networkIP)
+
+    def getMapNetworkDriveMRU(self):
+
+        output = self.volumeInfo
+        try:
+            bias = datetime.timedelta(hours=-self.bias)
+        except TypeError:
+            pass
+        if "FAT" or "NTFS" in output.split(" ")[0]:
+            os.chdir("%s/%s/" % (self.mountDir, output.split(" ")[2]))
+            logger.info("Loading every user info!")  # TODO:It should be per user!
+            try:
+                os.chdir("Users/")
+            except FileNotFoundError:
+                logger.error("Couldn't find Users folder!")
+                return None
+            for userDir in os.listdir("."):
+                if os.access("{0}/NTUSER.DAT".format(userDir), os.F_OK | os.R_OK):
+                    registry = Registry.Registry("{0}/NTUSER.DAT".format(userDir))
+                elif os.access("{0}/ntuser.dat".format(userDir), os.F_OK | os.R_OK):
+                    registry = Registry.Registry("{0}/ntuser.dat".format(userDir))
+                else:
+                    logger.warning("Couldn't find user registry on %s" % userDir)
+                    continue
+                open1 = registry.open("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Map Network Drive MRU")
+                logger.info("Showing Map Network Drive MRU info.")
+                for item in open1.values():
+                    if item.name() == "MRUList":
+                        continue
+                    logger.info(open1.value())
+    # https://digital-forensics.sans.org/blog/2011/07/05/shellbags
